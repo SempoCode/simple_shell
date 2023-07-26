303e3c59ad705f5fe829b4abdac13572c58f99fa
Lurhakongola <125456065+Lurhakongola@users.noreply.github.com>
2023-07-26 20:56:49 +0000

    add my changes

--- conversions.c
+++ conversions.c
@@ -2,7 +2,7 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: the string to be converted
+ * @s: String to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
 
